b8e041afd92c222c917a094bffe424c1029672a8
Nakul Thakur <thakurnakul119@gmail.com>
2024-01-27 17:56:00 +0530

    added personal page

--- src/Components/PersonalLinkPopup.jsx
+++ src/Components/PersonalLinkPopup.jsx
@@ -2,33 +2,29 @@ import axios from 'axios';
 import React, { useEffect } from 'react'
 import Popup from 'reactjs-popup'
 
-function GroupLinkPopup({setToggle,data,value}) {
-    async function updatelink(i,timing)
+function PersonalLinkPopup({setToggle,data,value}) {
+    async function updatelink()
     {
       setToggle({...data,message:"loading"})
-      console.log(data?.link[i])
-      console.log(value?.name)
-      console.log(timing)
-        setToggle({...data,message:"...loading"});
-        if(data?.link[i]!==undefined)
-      try {
-         const res=await axios.put(`https://shivaay-shakti-backend-1.onrender.com/api/purchase/grouplink`,{
-        //  const res=await axios.put(`http://localhost:5000/api/purchase/grouplink`,{
-          link:data?.link[i],
-          course_name:value?.name,
-          preferred_timing:timing
+      console.log(data?.link)
+      console.log(value?._id)
+      if(data?.link!=="")
+     { try {
+         const res=await axios.put(`https://shivaay-shakti-backend-1.onrender.com/api/purchase/personallink/${value?._id}`,{
+        //  const res=await axios.put(`http://localhost:5000/api/purchase/personallink/${value?._id}`,{
+          link:data?.link,
          },{ headers:{
           Authorization:localStorage.getItem('jwt')
         }})
         console.log(res);
         if(res.status===200)
-        setToggle({...data,message:"",toggle:false,link:[]});
+        setToggle({...data,message:"",toggle:false,link:""});
       } catch (e) {
         setToggle({...data,message:"server error"})
          console.log(e);
-      }
+      }}
       else{
-        setToggle({...data,message:"* Add Link"})
+        setToggle({...data,message:"* Add link"})
       }
     } 
   return (
@@ -39,7 +35,7 @@ function GroupLinkPopup({setToggle,data,value}) {
      closeOnEscape={false}
      contentStyle={{
        width: "40vw",
-       height: "60vh",
+       height: "30vh",
        overflow: "hidden",
        padding: "30px",
        backdropFilter: "blur(5px)",
@@ -53,25 +49,18 @@ function GroupLinkPopup({setToggle,data,value}) {
   <button onClick={()=>setToggle({...data,toggle:false,link:[]})} className=' text-xl font-bold absolute top-5 right-10'>X</button>
     <span className=' underline text-2xl font-bold'>Timings</span>
     <div className='flex flex-col mt-5  gap-y-5'>
-   {value?.group_timing?.map((v,i)=>
-   <>
    <div className='flex gap-6'>
-   <label className=' text-xl' htmlFor=""><span className='font-bold'>{i+1}.</span> {v?.times}</label>
-   <input value={data?.link[i]} onChange={(e)=>{data.link[i]=e.target.value;setToggle({...data})}} type="text" className='border border-black flex-1'/>
-   <button onClick={()=>updatelink(i,v?.times)} className=' bg-blue-500 hover:bg-white border hover:border-blue-500 hover:text-blue-500 text-white py-1 px-2 rounded-md'>Add</button>
+   <input value={data?.link} onChange={(e)=>{setToggle({...data,link:e.target.value})}} type="text" className='border border-black flex-1'/>
+   <button onClick={updatelink} className=' bg-blue-500 hover:bg-white border hover:border-blue-500 hover:text-blue-500 text-white py-1 px-2 rounded-md'>Add</button>
    </div>
-   </>
-   )}
    </div>
    <div className='text-center mt-5 text-red-500'>
    {data?.message}
    </div>
    </div>
-
-
    </Popup>
     </>
   )
 }
 
-export default GroupLinkPopup
+export default PersonalLinkPopup
--- src/Pages/Personal.jsx
+++ src/Pages/Personal.jsx
@@ -1,10 +1,66 @@
-import React from 'react'
-
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import GroupLinkPopup from '../Components/GroupLinkPopup';
+import PersonalLinkPopup from '../Components/PersonalLinkPopup';
 function Personal() {
+  const [data,setData]=useState({
+    data:[],
+    index:null,
+    // index1:null,
+    toggle:false,
+    message:"",
+    link:""
+  });
+  async function getdata()
+  {
+    try{
+  const response=await axios.get("https://shivaay-shakti-backend-1.onrender.com/api/purchase/personal",{ headers:{
+    Authorization:localStorage.getItem('jwt')
+  }});
+  console.log(response?.data?.data);
+   setData({...data,data:response?.data?.data});
+    }
+    catch(e)
+    {
+console.log(e);
+    }
+  }
+  useEffect(()=>{
+    getdata();
+  },[])
   return (
-    <div>
-      
-    </div>
+    <div className='  w-[100vw] h-screen overflow-y-auto  pl-[18vw]  pt-[14vh]  ' >
+    <div className=' w-[80vw]'>
+       <div className='  '>
+       <h1 className='  text-3xl font-semibold underline underline-offset-8  '>Group Courses</h1>
+       <table cellPadding={2} cellSpacing={2} className=' mt-8   text-left w-full   ' >
+         <tr className=' text-2xl border border-black bg-black  text-white p-2  '>
+           <th>Sr. No.</th>
+           <th>Name</th>
+           <th>Course Name</th>
+           <th>Timing</th>
+          <th></th>
+         </tr>
+      <div className='mt-4'></div>
+       {data?.data?.map((value,i)=>
+         <>
+         <tr className=' hover:bg-red-600 hover:text-white   text-[20px] font-semibold  border-2 border-black bg-gray-400 '>
+           <th className='   '>{i+1}.</th>
+         <td className=' capitalize '>{value?.name}</td>
+         <td className=' capitalize '>{value?.course_name}</td>
+         <td className=' capitalize '>{value?.preferred_timing}</td>
+        <td className=' space-x-5 '>
+          <button  onClick={()=>setData({...data,toggle:true,index:i})}  className=' hover:text-blue-700 underline  font-bold ' >Add Link</button>
+          {data?.index===i&&<PersonalLinkPopup data={data}   setToggle={setData}   value={value} />}
+          </td>
+         </tr>  
+         </>
+         )}
+          </table>
+     </div>  
+       
+   </div>
+   </div>
   )
 }
 
